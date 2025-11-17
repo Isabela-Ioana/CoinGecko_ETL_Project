b88f87da87827cf566d38fd81557f04767163370
Isabela-Ioana <mosneanu.ioana@gmail.com>
2025-11-17 14:59:55 +0100

    ETL

---
+++ extract.py
@@ -0,0 +1,42 @@
+import json
+import pandas as pd
+from datetime import datetime
+import requests
+
+
+#Read the key from the json file
+def load_api_key():
+    with open("config.json") as f:      #assures that the file will be closed
+        config= json.load(f)
+    return config["coingecko_api_key"]
+
+
+#Extract function
+def extract_crypto_data():
+    api_key=load_api_key()
+    url="https://api.coingecko.com/api/v3/coins/markets"
+    headers={
+        "x-cg-demo-api-key" : api_key
+    }
+
+    params={
+        "vs_currency" : "usd",
+        "order" : "market_cap_desc",
+        "per_page" : 50
+    }
+
+
+    response = requests.get(url,headers=headers,params=params)
+    if response.status_code!=200:       #200 is the code for a successful GET
+        raise Exception(f"API request failed with code: {response.status_code}")
+    
+    data= response.json()     #.json transforms in Python Objects -> dictionary
+    df= pd.json_normalize(data)   #.json_normalize transforms in a DataFrame
+    df["etl_timestamp"]=datetime.now()   #we add a column representing the time when we did the extraction
+    return df
+
+
+if __name__=="__main__":
+    df= extract_crypto_data()
+    print(df.head())
+
---
+++ transform.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from extract import extract_crypto_data
+
+def transform_crypto_data(df):
+    columns_to_keep=[
+        "id","symbol","name","current_price","market_cap","total_volume",
+        "price_change_24h", "price_change_percentage_24h", "last_updated",
+        "etl_timestamp"
+    ]
+    df=df[columns_to_keep]
+
+    #transformation to numeric and date
+
+                                    # df["current_price"]= pd.to_numeric(df["current_price"],errors="coerce")  #coerce= any non-numeric values transformed in NaN
+                                    # df["market_cap"]=pd.to_numeric(df["market_cap"],errors="coerce")
+                                    # df["total_volume"]=pd.to_numeric( df["total_volume"],errors="coerce")
+                                    # df["price_change_24h"]= pd.to_numeric(df["price_change_24h"],errors="coerce")
+
+    columns_tobenumeric=["current_price","market_cap","total_volume",
+       "price_change_24h", "price_change_percentage_24h"]
+    for col in columns_tobenumeric:
+        df[col]=pd.to_numeric(df[col],errors="coerce")
+
+    columns_tobedate=["last_updated", "etl_timestamp"]
+    for col in columns_tobedate:
+        df[col]=pd.to_datetime(df[col],errors="coerce")
+
+    df.dropna(subset=["id", "symbol", "current_price"],inplace=True)
+
+    return df
+
+if __name__=="__main__":
+    
+    df_extracted= extract_crypto_data()
+
+    df_cleaned= transform_crypto_data(df_extracted)
+    print(df_cleaned.head())
