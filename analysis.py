4dffe6eae5cc3e419a94186d2673bc2fd23fe17f
Isabela-Ioana <mosneanu.ioana@gmail.com>
2025-11-20 17:55:17 +0100

    Updated the descriptive_analysis

--- analysis.py
+++ analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
-df = pd.read_csv("crypto_data.csv")
+df = pd.read_csv("csv_backup/crypto_data.csv")
 
 #Descriptive statistics - see descriptive_analysis.txt
 print(df.describe())
